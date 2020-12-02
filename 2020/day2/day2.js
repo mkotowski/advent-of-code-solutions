c86ff3ef55bf843ecc23b890cd320188534ae5c4
Michał Kotowski <dev@kotowski.xyz>
2020-12-02 09:09:13 +0100

    Remove unnecessary loops and improve variable names

--- 2020/day2/day2.js
+++ 2020/day2/day2.js
@@ -1,53 +1,43 @@
 const fs = require('fs');
 
-var properPasswordsCount = 0;
-var secondCount = 0;
-
-class Record {
-	constructor(min, max, letter, password) {
-		this.min = min;
-		this.max = max;
-		this.letter = letter;
-		this.password = password;
-	}
-}
+var sledRentalPassCount = 0;
+var tobogganRentalPassCount = 0;
 
 fs.readFile("input.txt", 'utf8', function (err, data) {
-        if (err) {
-                return console.log(err);
-        }
+	if (err) {
+		return console.log(err);
+	}
 
-	let rawRecords = data.trim().split('\n');
+	let records = data.trim().split('\n');
 
-	let records = [];
-
-	for (let rawRecord of rawRecords) {
-		let requirementsAndPassword = rawRecord.split(/[: \-\n]+/);
+	for (let record of records) {
+		let requirementsAndPassword = record.split(/[: \-\n]+/);
+		
 		let min = parseInt(requirementsAndPassword[0], 10);
 		let max = parseInt(requirementsAndPassword[1], 10);
 		let letter = requirementsAndPassword[2];
 		let password = requirementsAndPassword[3];
 
-		records.push(new Record(min, max, letter, password));
-	}
-	
-	for (let record of records) {
-		let regex = new RegExp(record.letter, 'g');
-		let count = (record.password.match(regex) || []).length
+		// the sled rental password policy check
+		let regex = new RegExp(letter, 'g');
+		let count = (password.match(regex) || []).length
 
-		if (count >= record.min && count <= record.max) {
-			properPasswordsCount++;
+		if (count >= min && count <= max) {
+			sledRentalPassCount++;
 		}
 		
-		// the second part
-		let firstPosition = (record.password[record.min-1] == record.letter);
-		let secondPosition = (record.password[record.max-1] == record.letter);
+		// the North Pole Toboggan Rental Shop password policy check
+		let firstPosition  = (password[min-1] == letter);
+		let secondPosition = (password[max-1] == letter);
 
-		if ((firstPosition != secondPosition) && (firstPosition || secondPosition)) {
-			secondCount++;
+		let doLetterExists      = (firstPosition || secondPosition)
+		let isLetterNotRepeated = (firstPosition != secondPosition)
+
+		if (doLetterExists && isLetterNotRepeated) {
+			tobogganRentalPassCount++;
 		}
 	}
 
-	console.log(properPasswordsCount);
-	console.log(secondCount);
+	console.log(sledRentalPassCount);
+	console.log(tobogganRentalPassCount);
 });
